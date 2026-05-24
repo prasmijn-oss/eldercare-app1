7d2245939db975c4d181c9bf4c2fe471301bc93d
prasmijn-oss <p.rasmijn@gmail.com>
2026-05-23 20:10:59 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect, useCallback, useRef } from "react";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.49.0";
 
-const SUPABASE_URL = "https://arwvosghwecyzpqartrh.supabase.co";
-const ANON_KEY = "sb_publishable_hQS1UpNxtIke2N-WICNKlg_ruIPrOeN";
-const SERVICE_KEY = import.meta.env.SUPABASE_SECRET_KEY||import.meta.env.VITE_SUPABASE_SERVICE_KEY||"";
+const SUPABASE_URL = "https://kpwzeawgrqdsezflvjkm.supabase.co";
+const ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtwd3plYXdncnFkc2V6Zmx2amttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1Mzc1MzIsImV4cCI6MjA5NTExMzUzMn0.-fvmwgZqwyddWyq1IJ4vcHvsTVMpPmhI72p4hyCtC6E";
+const SERVICE_KEY = "";
 
 const supabase = createClient(SUPABASE_URL, ANON_KEY);
 const supabaseAdmin = SERVICE_KEY
